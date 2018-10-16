932cbc463c6113b60ed80eb5780553f195db28a0
ThuF <jordan.p.pavlov@gmail.com>
2018-10-16 17:30:20 +0300

    Updates REST API

--- bookshop-print-on-demand/api.js
+++ bookshop-print-on-demand/api.js
@@ -1,6 +1,18 @@
+var rs = require('http/v3/rs');
+var dao = require('bookshop-print-on-demand/data/dao/PrintRequests');
 var process = require('bpm/v3/process');
 
-process.start('bookshop-print-on-demand', {
-	'title': 'Title',
-	'address': 'Address'
-});
+rs.service()
+	.resource('')
+		.get(function(ctx, request, response) {
+			response.println(JSON.stringify(dao.list()));
+		})
+	.resource('')
+		.post(function(ctx, request, response) {
+			var printRequest =  request.getJSON();
+			process.start('bookshop-print-on-demand', {
+				'title': printRequest.Title,
+				'address': printRequest.Address
+			});
+		})
+.execute();
